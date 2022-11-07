6f859cdd6f756055984b7d3580943b45e517e5cc
hayratjan <1693305172@qq.com>
2022-11-07 18:52:42 +0800

    SimpleMiddleware.py

--- 学生公寓报修管理系统的设计与实现/SimpleMiddleware.py
+++ 学生公寓报修管理系统的设计与实现/SimpleMiddleware.py
@@ -16,7 +16,7 @@ class SimpleMiddleware:
             1.判断是否登录
         """
         # 允许后台不登录情况下访问的路径
-        urllist = ['/', '/logout/', '/login/', '/register/', '/admin/']
+        urllist = ['/', '/logout/', '/login/', '/register/', '/admin/', '/admin/login/']
         if path not in urllist:
             # 重定向到登录页
             if 'name' not in request.session:
